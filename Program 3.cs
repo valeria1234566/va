c4da4c26d111c2f9c1a706d848f09b0064ecc6f4
valeria1234566 <72233152+valeria1234566@users.noreply.github.com>
2023-11-08 09:41:02 +0300

    Add files via upload

---
+++ Program 3.cs
@@ -0,0 +1,52 @@
+﻿```csharp
+using System;
+using System.Collections.Generic;
+
+namespace DTPExample
+{
+    // Базовый класс для автомобиля
+    public class Car
+    {
+        public string LicensePlate { get; set; }
+    }
+
+    // Класс для ДТП
+    public class DTP<T> where T : Car
+    {
+        public List<T> Participants { get; set; }
+
+        public DTP(List<T> participants)
+        {
+            Participants = participants;
+        }
+
+        public void PrintParticipants()
+        {
+            Console.Write("Список участников ДТП: ");
+            foreach (var participant in Participants)
+            {
+                Console.Write(participant.LicensePlate + ", ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание экземпляров автомобилей
+            Car car1 = new Car { LicensePlate = "A123BC" };
+            Car car2 = new Car { LicensePlate = "B456DE" };
+            Car car3 = new Car { LicensePlate = "C789FG" };
+
+            // Создание экземпляра ДТП с автомобилями в качестве участников
+            DTP<Car> dtp = new DTP<Car>(new List<Car> { car1, car2, car3 });
+
+            // Вывод списка участников ДТП
+            dtp.PrintParticipants();
+
+            Console.ReadLine();
+        }
+    }
+}
